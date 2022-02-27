673e4289e40cd3e88b84990b54c99611b0b26375
Kudo A <82983113+AK-K1014@users.noreply.github.com>
2022-02-27 16:53:38 +0900

    Add files via upload

--- memo.rb
+++ memo.rb
@@ -1,34 +1,36 @@
- require "csv"
+require "csv"
  
-    puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+  puts "1(新規でメモを作成) 2(既存のメモ編集する)"
  
-    memo_type = gets.chomp
+  memo_type = gets.chomp
  
 if memo_type == "1"
-    puts "拡張子を除いたファイルを入力してください。"
+  puts "拡張子を除いたファイルを入力してください。"
 
-    file_name = gets.chomp
+  file_name = gets.chomp
  
-    puts "メモしたい内容を記入してください"
-    puts "完了したらCtrl + Dをおします"
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dをおします"
 
-    memo_type = STDIN.read
+  memo_type = STDIN.read
 
-    CSV.open("#{file_name}.csv","w") do |memo|
+  CSV.open("#{file_name}.csv","w") do |memo|
         memo << ["#{memo_type}"]
- end
+  end
 
 elsif memo_type == "2"
 
-    file_name = gets.chomp
-    
-    puts "編集内容を入力してください"
-    puts "終了後、Ctrl + Dをおします"
+  file_name = gets.chomp
 
-    memo_type = STDIN.read
+  puts "編集したいファイル名を入力して下さい"
+  puts "編集内容を入力してください"
+  puts "終了後、Ctrl + Dをおします"
 
-    CSV.open("#{file_name}.csv","a") do |memo|
+  memo_type = STDIN.read
+
+  CSV.open("#{file_name}.csv","a") do |memo|
         memo << ["#{memo_type}"]
- end
-    
+  end
+else 
+  puts "処理を終了"
 end
